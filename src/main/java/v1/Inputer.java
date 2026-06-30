34bfa128d2c102117ddd5d919d019d86a8b2570c
Tatsuhiro Tsuchiya <t-tutiya@ist.osaka-u.ac.jp>
2026-06-30 10:34:17 +0900

    Update Input.java
    Add an import sentence but nothing has been done with it

--- src/main/java/v1/Inputer.java
+++ src/main/java/v1/Inputer.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
